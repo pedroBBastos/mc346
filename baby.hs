d8368c75e9a65ac7433575ebbd1648b239438c4e
Pedro Bastos <pedro.bbastos123@gmail.com>
2018-08-06 17:15:17 -0300

    Baby steps to haskell knowledge

---
+++ baby.hs
@@ -0,0 +1,11 @@
+doubleMe x = x + x
+
+doubleUs x y = x*2 + y*2
+
+meninoMaroto x y = doubleMe x + doubleMe y
+
+funcaoComIf y = if y > 100
+                then y
+                else y*2
+
+funcApostrofe' g = (if g >= 13 then "sdsd" else "Lalaland")
