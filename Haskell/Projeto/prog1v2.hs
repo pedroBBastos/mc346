50b707cb480dc5c7ff3b4830065adba602582a9b
Pedro Bastos <pedro.bbastos123@gmail.com>
2018-09-30 18:34:16 -0300

    Adicionando comentários.

--- Haskell/Projeto/prog1v2.hs
+++ Haskell/Projeto/prog1v2.hs
@@ -1,32 +1,40 @@
-import Data.Graph
-import Data.Maybe
-import qualified Data.Set as Set
+-- Pedro Barros Bastos      RA : 204481
+-- Rafael Ferreira Galib    RA : 204904
 
+import Data.Graph -- estrutura padrao que eh usada para representar um grafo
+import Data.Maybe
+import qualified Data.Set as Set -- -- estrutura padrao que eh usada como uma fila de prioridade
+
+-- Tripla : tipo criado para melhor se trabalhar com triplas. Usado para criacao das adjacencias.
 data Tripla = Tripla { node :: String, key :: String, adj :: [String]} deriving (Show, Eq)
+-- Traslado : tipo para representar um modo de caminho e o tempo deste.
 data Traslado = Traslado { modo :: String, tempo :: Float} deriving (Show, Eq)
+-- Distancia : tipo para representar distancia no grafo.
 data Distancia = Infinita | Distancia { valorDistancia :: Float} deriving (Eq)
+-- ItemFilaPrioridade : tipo que representa um item na fila de prioridade que eh usada durante o dijkstra.
 data ItemFilaPrioridade = ItemFilaPrioridade {distanciaAteAqui :: Distancia, verticeAtual :: Vertex, verticeAnterior :: (Maybe Vertex, Maybe String)} deriving (Show, Eq)
 
-instance Ord Distancia where
+instance Ord Distancia where -- implementando a forma de ordenacao entre tipos Distancia
   compare Infinita (Distancia _) = GT
   compare (Distancia _) Infinita = LT
   compare Infinita Infinita = EQ
   compare (Distancia a) (Distancia b) = compare a b
 
-instance Ord ItemFilaPrioridade where
+instance Ord ItemFilaPrioridade where -- implementando a forma de ordenacao entre tipos ItemFilaPrioridade
   compare a b = if comparaDistancias == EQ then compare (verticeAtual a) (verticeAtual b) else comparaDistancias
       where comparaDistancias = compare (distanciaAteAqui a) (distanciaAteAqui b)
 
-instance Show Distancia where
+instance Show Distancia where -- implementando a forma de exibicao entre tipos Distancia
   show Infinita = "Infinita"
   show (Distancia d) = show d
 
 -----------------------------------------
 
-main = interact process
+main = interact process -- main
 
 -----------------------------------------
 
+-- funcao criada para processar entrada e retornar uma saida.
 process :: String -> String
 process input =
   let allLines = lines input
@@ -38,6 +46,7 @@ process input =
       result = (saida dijkstraResult nodeFromVertex) ++ "\n"  ++ (show $ distanciaAteAqui $ last dijkstraResult) ++ "\n"
   in result
 
+-- processa a entrada e retorna uma lista de lista de string.
 processaEntrada :: [String] -> [[String]]
 processaEntrada [] = []
 processaEntrada entrada = let e = takeWhile (/= "") entrada
@@ -45,6 +54,7 @@ processaEntrada entrada = let e = takeWhile (/= "") entrada
                               r' = if length r == 0 then r else tail r
                           in [e] ++ processaEntrada r'
 
+-- cria triplas correspondentes a primeira parte da entrada, as adjacencias e seus pesos.
 criaTriplas :: [String] -> [Tripla]
 criaTriplas listaAdjacencias = foldl (\acc x -> add (take 2 $ words x) acc) [] listaAdjacencias
     where add lAdj@[origem, destino] vTripla = adicionaAresta (foldl (\acc x -> if pegaVertice x acc == Nothing
@@ -58,9 +68,11 @@ criaTriplas listaAdjacencias = foldl (\acc x -> add (take 2 $ words x) acc) [] l
                     | otherwise = t : adicionaAresta ts
           pegaVertice c vTripla = foldl (\acc t -> if node t == c then Just t else acc) Nothing vTripla
 
+-- funcao que converte uma lista do tipo Tripla para uma lista de triplas padrao.
 paraTriplaConvencional :: [Tripla] -> [(String, String, [String])]
 paraTriplaConvencional vTripla = foldl (\acc t -> acc ++ [(node t, key t, adj t)]) [] vTripla
 
+-- cria um vetor de tuplas que relaciona cada aresta do grafo com seus possiveis caminhos
 criaTabelaDeCaminhos :: [String] -> (String -> Maybe Vertex) -> [(Edge, [Traslado])]
 criaTabelaDeCaminhos listaAdjacencias vertexFromKey = foldl (\acc x -> atualizaCaminhos x acc) [] listaAdjacencias
     where atualizaCaminhos adj tabela =
@@ -73,6 +85,7 @@ criaTabelaDeCaminhos listaAdjacencias vertexFromKey = foldl (\acc x -> atualizaC
                       | e == fst t1 = (e, snd t1 ++ [Traslado {modo=m, tempo=t}]) : resto
                       | otherwise = t1 : updateTable e m t resto
 
+-- funcao que roda o dijkstra a partir do grafo criado.
 dijkstra :: (Graph, (Vertex -> (String, String, [String])), (String -> Maybe Vertex)) -> [(Edge, [Traslado])] -> [(String, Float)] -> Vertex -> Vertex -> [ItemFilaPrioridade]
 dijkstra (graph, nodeFromVertex, vertexFromKey) tabelaAdjacencias periodoOnibus origem destino =
     rodaDijkstra (constroiFilaDePrioridade (vertices graph) origem) origem []
@@ -84,16 +97,20 @@ dijkstra (graph, nodeFromVertex, vertexFromKey) tabelaAdjacencias periodoOnibus 
                               topo = Set.elemAt 0 f
                               f' = atualizaFilaPrioridade f topo subsetAdj' periodoOnibus
                               f'' = Set.deleteAt 0 f'
-                          in topo : (rodaDijkstra f'' (verticeAtual $ Set.elemAt 0 f'') (c ++ [v])) -- chamar recursao aqui
+                          in topo : (rodaDijkstra f'' (verticeAtual $ Set.elemAt 0 f'') (c ++ [v]))
 
+-- cria um vetor de tuplas para relacionar as linhas de onibus com seus respectivos periodos de saida.
 periodosOnibus :: [String] -> [(String, Float)]
 periodosOnibus v = foldl (\acc p -> let [linha, tempoEspera] = words p in acc ++ [(linha, read tempoEspera)]) [] v
 
+-- dado o retorno do djikstra, monta a string de saida para exibir o menor caminho calculado.
 saida :: [ItemFilaPrioridade] -> (Vertex -> (String, String, [String])) -> String
 saida l nodeFromVertex = tail $ fst $ foldr (\i acc -> if snd acc == Nothing || (fromJust $ snd acc)  == verticeAtual i
                                                         then ((fromMaybe "" (snd $ verticeAnterior i)) ++ " " ++ (pegaIdVertice $ verticeAtual i) ++ " " ++ fst acc, fst $ verticeAnterior i)
                                                         else acc) ("", Nothing) l
     where pegaIdVertice v = let (label, _, _) = nodeFromVertex v in label
+
+-- as funcoes abaixo operam sobre a fila de prioridade, seja para manipula-la ou para criar um novo item, etc
 
 novoItemFilaPrioridade :: Distancia -> Vertex -> (Maybe Vertex, Maybe String) -> ItemFilaPrioridade
 novoItemFilaPrioridade d ve va = ItemFilaPrioridade {distanciaAteAqui=d, verticeAtual=ve, verticeAnterior=va}
@@ -109,6 +126,7 @@ constroiFilaDePrioridade listaVertices vOrigem = foldl (\acc v -> if v == vOrige
                                                                     then Set.insert (novoItemFilaPrioridade (Distancia 0.0) v (Nothing, Nothing)) acc
                                                                     else Set.insert (novoItemFilaPrioridade Infinita v (Nothing, Nothing)) acc) (Set.fromList []) listaVertices
 
+-- onde o efetivo calculo do menor caminho eh feito a partir da lista adjacencias de um dado vertice
 atualizaFilaPrioridade :: (Set.Set ItemFilaPrioridade) -> ItemFilaPrioridade -> [(Edge, [Traslado])] -> [(String, Float)] -> (Set.Set ItemFilaPrioridade)
 atualizaFilaPrioridade fp atual adjs po = foldl (\acc e -> let item = encontraItemPorVertice (snd $ fst e) acc
                                                                melhorTraslado = fromJust $ menorTraslado $ snd e
