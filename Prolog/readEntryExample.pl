0fcf2f99101a817cbe646bad2bd6c14d1a6b0a0c
Pedro Bastos <pedro.bbastos123@gmail.com>
2018-10-09 15:36:20 -0300

    Predicado que retorna o que há de comum entre o final de uma string e o início de outra.

--- Prolog/readEntryExample.pl
+++ Prolog/readEntryExample.pl
@@ -25,5 +25,25 @@ entradas(S) :-
 % para o projeto, teriamos que fazer alguma iteracao O(n^l), sendo l o
 % numero de linhas recebidas da entrada e verificar as melhores possibilidades
 % de join.
-%
-% Pensar numa maneira boa de comparar inicio/fim das strings
+
+% uso do substring -> http://www.swi-prolog.org/pldoc/man?predicate=sub_string/5
+% sub_string(+String, ?Before, ?Length, ?After, ?SubString)
+sub_string("minha rua eh dahorinha", 2, 3, X, L).
+
+% tentar primeiro como determinar como colar duas strings
+
+% para pegar ultimo caracter de string
+sub_string("abdef", _, 1, 0, C).
+
+% http://www.swi-prolog.org/pldoc/man?predicate=string_length/2
+
+% predicado que retorna o que ha em comum entre o fim de S1 e o inicio de S2
+commonForMe(S1, S2, R) :-
+    sub_string(S1, _, 1, 0, LastCharS1),
+    sub_string(S2, _, 1, 0, LastCharS2),
+    (LastCharS1 = LastCharS2
+    ->  sub_string(S1, 0, _, 1, WithoutLastChar1),
+        sub_string(S2, 0, _, 1, WithoutLastChar2),
+        commonForMe(WithoutLastChar1, WithoutLastChar2, RR),
+        string_concat(RR, LastCharS1, R);
+    R = "").
