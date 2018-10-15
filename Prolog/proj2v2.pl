693ffe6cc1d98a6399c10c672457b062b857c4eb
Pedro Bastos <pedro.bbastos123@gmail.com>
2018-10-15 20:50:11 -0300

    Tentando fazer de outro modo para pegar as maiores combinações. Não rolou. Talvez pensar em usar arvore e etc

--- Prolog/proj2v2.pl
+++ Prolog/proj2v2.pl
@@ -37,5 +37,28 @@ p(G, [H|T], NovoAcc) :-
      NovoAcc = [R1|T]).
 
 montaGenoma(L, R) :- foldl(p, L, [], R).
-
 %montaGenoma(["xxxxxababababyyyyyy", "yyaaaaaaaaaaa", "yyyyyyeeeeeeeeeeeeee", "cccccccccccccccxxxxx", "fffffffffffffffwwwwww", "wwwwwwgggggggggggxx"], X).
+
+% -----------------------------------------
+% ISSO AQUI EM BAIXO ESTÁ DANDO ERRADO
+% -> depurando, ainda acaba nao analisando todos os possiveis casos para poder
+% comparar o tamanho. Aqui em baixo a nao comparação ocorre na volta da recursao
+
+p2(E, [], E) :- !, true.
+p2(E, [H|T], M) :-
+    combine(H, E, R1),
+    (R1 = "" ->  combine(E, H, R2),
+        (R2 = "" -> M = H;
+         p2(E, T, RR),
+         string_length(R2, L21), string_length(RR, L22),
+         (L21 > L22 -> M = R2; M = RR));
+     p2(E, T, RR),
+     string_length(R1, L11), string_length(RR, L12),
+     (L11 > L12 -> M = R1; M = RR)).
+
+montaGenoma2([], []).
+montaGenoma2([H|T], R) :- montaGenoma2(T, RR), p2(H, T, M), R = [M|RR].
+
+% -----------------------------------------
+% talvez seja algo como construir uma arvore com as combinacaoes possiveis
+% e pegar a que possui a maior combinacao. Olhar os algoritmos da aula 16 pra ver se ajuda
