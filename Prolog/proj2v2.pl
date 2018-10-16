d74c0f1fb51b0d3b16aeebc0aaa6ead91f258a17
Pedro Bastos <pedro.bbastos123@gmail.com>
2018-10-16 20:14:28 -0300

    Conforme conversado com o professor, não eh preciso analisar estes casos de dupla ligação por estar definido que haverá no máximo apenas 1 ligação possível a ser feita. O que foi feito até o momento serve.

--- Prolog/proj2v2.pl
+++ Prolog/proj2v2.pl
@@ -20,13 +20,13 @@ combine(S1, S2, R) :-
     commonForMe(S1, S2, 1, [], V),
     head(V, H),
     string_length(H, LengthCommon),
-    (  LengthCommon > 0
+    (  LengthCommon > 3
     -> sub_string(S2, LengthCommon, _, 0, S2WithoutCommon), string_concat(S1, S2WithoutCommon, R);
     R = "").
 
 % -----------------------------------------
 
-%P( +Genoma, +Acumulador, -NovoAcum)
+%p( +Genoma, +Acumulador, -NovoAcum)
 p(G, [], [G]) :- !, true.
 p(G, [H|T], NovoAcc) :-
     combine(H, G, R1),
@@ -38,27 +38,3 @@ p(G, [H|T], NovoAcc) :-
 
 montaGenoma(L, R) :- foldl(p, L, [], R).
 %montaGenoma(["xxxxxababababyyyyyy", "yyaaaaaaaaaaa", "yyyyyyeeeeeeeeeeeeee", "cccccccccccccccxxxxx", "fffffffffffffffwwwwww", "wwwwwwgggggggggggxx"], X).
-
-% -----------------------------------------
-% ISSO AQUI EM BAIXO ESTÁ DANDO ERRADO
-% -> depurando, ainda acaba nao analisando todos os possiveis casos para poder
-% comparar o tamanho. Aqui em baixo a nao comparação ocorre na volta da recursao
-
-p2(E, [], E) :- !, true.
-p2(E, [H|T], M) :-
-    combine(H, E, R1),
-    (R1 = "" ->  combine(E, H, R2),
-        (R2 = "" -> M = H;
-         p2(E, T, RR),
-         string_length(R2, L21), string_length(RR, L22),
-         (L21 > L22 -> M = R2; M = RR));
-     p2(E, T, RR),
-     string_length(R1, L11), string_length(RR, L12),
-     (L11 > L12 -> M = R1; M = RR)).
-
-montaGenoma2([], []).
-montaGenoma2([H|T], R) :- montaGenoma2(T, RR), p2(H, T, M), R = [M|RR].
-
-% -----------------------------------------
-% talvez seja algo como construir uma arvore com as combinacaoes possiveis
-% e pegar a que possui a maior combinacao. Olhar os algoritmos da aula 16 pra ver se ajuda
