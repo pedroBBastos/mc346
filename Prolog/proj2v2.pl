3f1f8d7f608b63497098455fcbf2ba4e9f61ca15
Pedro Bastos <pedro.bbastos123@gmail.com>
2018-10-20 16:23:29 -0300

    Reorganizando código e colocando correção para fazer as montagens de genoma enquanto for possível fazê-las

--- Prolog/proj2v2.pl
+++ Prolog/proj2v2.pl
@@ -1,9 +1,47 @@
+main :-
+    entrada(X),
+    transf(X, S),
+    processa(S, R),
+    saida(R).
+
+% -----------------------------------------
+
+% predicado para montar vetor com linhas da entrada
+entrada([H|T]):- read_line_to_codes(user_input,H), H \= end_of_file, entrada(T).
+entrada([]).
+
+transf([H], R) :- string_codes(String, H), R = [String].
+transf([H|T], R) :- string_codes(String, H), transf(T, RR), append([String], RR, R).
+
+saida([H|T]) :- write("\n"), write(H), saida(T).
+saida([]).
+
+% -----------------------------------------
+
+processa(S, R) :-
+  montaGenoma(S, RR),
+  length(S, LI), length(RR, LF),
+  (LI = LF -> R = RR; processa(RR, R)).
+
+%p( +Genoma, +Acumulador, -NovoAcum)
+p(G, [], [G]) :- !, true.
+p(G, [H|T], NovoAcc) :-
+    combine(H, G, R1),
+    (R1 = "" ->  combine(G, H, R2),
+        (R2 = "" ->  p(G, T, PP), NovoAcc = [H|PP];
+         NovoAcc = [R2|T]
+        );
+     NovoAcc = [R1|T]).
+
+montaGenoma(L, R) :- foldl(p, L, [], R).
+
+% -----------------------------------------
+
 prepend(L, E, [E|L]).
 
 head([], "").
 head([H|_], H).
 
-% commonForMe("abcfcf", "cfcfdd", 1, [], X)
 commonForMe(S1, S2, L, Acc, R) :-
     string_length(S1, L1), string_length(S2, L2),
     (L > L1 ->  R = Acc;
@@ -23,36 +61,3 @@ combine(S1, S2, R) :-
     (  LengthCommon > 3
     -> sub_string(S2, LengthCommon, _, 0, S2WithoutCommon), string_concat(S1, S2WithoutCommon, R);
     R = "").
-
-% -----------------------------------------
-
-%p( +Genoma, +Acumulador, -NovoAcum)
-p(G, [], [G]) :- !, true.
-p(G, [H|T], NovoAcc) :-
-    combine(H, G, R1),
-    (R1 = "" ->  combine(G, H, R2),
-        (R2 = "" ->  p(G, T, PP), NovoAcc = [H|PP];
-         NovoAcc = [R2|T]
-        );
-     NovoAcc = [R1|T]).
-
-montaGenoma(L, R) :- foldl(p, L, [], R).
-%montaGenoma(["xxxxxababababyyyyyy", "yyaaaaaaaaaaa", "yyyyyyeeeeeeeeeeeeee", "cccccccccccccccxxxxx", "fffffffffffffffwwwwww", "wwwwwwgggggggggggxx"], X).
-
-% -----------------------------------------
-
-% predicado para montar vetor com linhas da entrada
-entrada([H|T]):- read_line_to_codes(user_input,H), H \= end_of_file, entrada(T).
-entrada([]).
-
-transf([H], R) :- string_codes(String, H), R = [String].
-transf([H|T], R) :- string_codes(String, H), transf(T, RR), append([String], RR, R).
-
-saida([H|T]) :- write("\n"), write(H), saida(T).
-saida([]).
-
-main :-
-    entrada(X),
-    transf(X, S),
-    montaGenoma(S, R),
-    saida(R).
